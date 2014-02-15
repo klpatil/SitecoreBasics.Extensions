9fc3e78de3514fdfa781d2ff4effc286a7947782
Kiran Patil <klpatil@hotmail.com>
2014-02-16 00:21:57 +0530

    Initial version

---
+++ SitecoreBasics.Extensions.Sourcecode/SitecoreBasics.Extensions/ItemClassExtensions.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Data.Items;
+using Sitecore.Globalization;
+using Sitecore.Data.Fields;
+
+namespace SitecoreBasics.Extensions
+{
+    /// <summary>
+    /// This class will be used to
+    /// provide extension methods for Item class
+    /// </summary>
+    public static class ItemClassExtensions
+    {
+        /// <summary>
+        /// This extension method will be used to check whether item
+        /// exist in current Sitecore Context language or not
+        /// </summary>
+        /// <param name="item">Item to check version for</param>
+        /// <returns>true if exist else false</returns>
+        public static bool IsLanguageVersionExist(this Item item)
+        {
+            bool isExist = false;
+            Sitecore.Data.Items.Item itemInLanguage =
+                item.Database.GetItem(item.ID, Sitecore.Context.Language);
+            isExist = itemInLanguage.Versions.Count > 0;
+            return isExist;
+        }
+
+        /// <summary>
+        /// This extension method will be used to check whether item
+        /// exist in current provided language or not
+        /// </summary>
+        /// <param name="item">Item to check version for</param>
+        /// <param name="languageToCheck">Language version to check for</param>
+        /// <returns>true if exist else false</returns>
+        public static bool IsLanguageVersionExist(this Item item, Language languageToCheck)
+        {
+            bool isExist = false;
+            Sitecore.Data.Items.Item itemInLanguage =
+                item.Database.GetItem(item.ID, languageToCheck);
+            isExist = itemInLanguage.Versions.Count > 0;
+            return isExist;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get Boolean value from provided field.
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static bool GetBooleanField(this Item item, string fieldName)
+        {
+            Field fieldValue = item.Fields[fieldName];
+
+            if (fieldValue != null)
+                return (fieldValue.Value == "1" ? true : false);
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get media item value from provided field.
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static MediaItem GetMediaItemField(this Item item, string fieldName)
+        {
+            Field fieldValue = item.Fields[fieldName];
+            MediaItem mediaItem = null;
+
+            if ((fieldValue != null) && (fieldValue.HasValue))
+                mediaItem = ((ImageField)fieldValue).MediaItem;
+
+            return mediaItem;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get string value from provided field.
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static String GetStringField(this Item item, String fieldName)
+        {
+            string fieldValue = string.Empty;
+            if (item != null)
+            {
+                Field field = item.Fields[fieldName];
+                if ((field != null) && (!string.IsNullOrEmpty(field.Value)))
+                    fieldValue = field.Value;
+            }
+            return fieldValue;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get date value from provided field.
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static DateTime GetDateField(this Item item, String fieldName)
+        {
+            DateField fieldValue = item.Fields[fieldName];
+            DateTime dateTime = DateTime.Now;
+            if ((fieldValue != null) && (fieldValue.InnerField.Value != null))
+                dateTime = fieldValue.DateTime;
+
+            return dateTime;
+        }
+        
+        /// <summary>
+        /// This extension method will be used to 
+        /// get HtmlField value from provided field.
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static HtmlField GetHtmlField(this Item item, String fieldName)
+        {
+            HtmlField htmlField = item.Fields[fieldName];
+            return htmlField;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get LinkField value from provided field.
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static LinkField GetLinkField(this Item item, String fieldName)
+        {   
+            LinkField linkField = item.Fields[fieldName];
+            return linkField;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get ReferenceField value from provided field.
+        /// Useful for : Droplink, Droptree, Grouped Droplink
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static ReferenceField GetReferenceField(this Item item, String fieldName)
+        {            
+            ReferenceField referenceField = item.Fields[fieldName];
+            return referenceField;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get ReferenceField Item's reference from provided field.
+        /// Useful for : Droplink, Droptree, Grouped Droplink
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static Item GetReferenceFieldItem(this Item item, String fieldName)
+        {
+            Item targetItem = null;
+            ReferenceField referenceField = item.Fields[fieldName];
+            if ((referenceField != null) && (referenceField.TargetItem != null))
+                targetItem = referenceField.TargetItem;
+
+            return targetItem;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get MultilistField from provided field.
+        /// Useful for : Checklist, Multilist, Treelist, Treelist-Ex
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static MultilistField GetMultilistField(this Item item, String fieldName)
+        {
+            MultilistField multilistField = item.Fields[fieldName];
+            return multilistField;
+        }
+
+        /// <summary>
+        /// This extension method will be used to 
+        /// get MultilistField items from provided field.
+        /// Useful for : Checklist, Multilist, Treelist, Treelist-Ex
+        /// </summary>
+        /// <param name="item">Item instance</param>
+        /// <param name="fieldName">Name of the field</param>
+        /// <returns>value of the field</returns>
+        public static Item[] GetMultilistFieldItems(this Item item, String fieldName)
+        {            
+            Item[] targetItems = null;
+            MultilistField multilistField = item.Fields[fieldName];
+            if ((multilistField != null) && (multilistField.InnerField != null))
+                targetItems = multilistField.GetItems();
+
+            return targetItems;
+        }
+        
+
+    }
+}
---
+++ SitecoreBasics.Extensions.Sourcecode/SitecoreBasics.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SitecoreBasics.Extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SitecoreBasics")]
+[assembly: AssemblyProduct("SitecoreBasics.Extensions")]
+[assembly: AssemblyCopyright("Copyright © SitecoreBasics 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6dcc10e6-d80b-46fe-89d0-23cdc3851f98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
